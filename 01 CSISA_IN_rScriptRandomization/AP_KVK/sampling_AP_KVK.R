fdcbaf175b55071682f2db2755b364e1fcca3ba9
CSISA Project <52654890+CSISAProject@users.noreply.github.com>
2019-07-17 14:11:26 +0545

    Update sampling_AP_KVK.R
    Added comments

--- 01 CSISA_IN_rScriptRandomization/AP_KVK/sampling_AP_KVK.R
+++ 01 CSISA_IN_rScriptRandomization/AP_KVK/sampling_AP_KVK.R
@@ -1,4 +1,5 @@
-library(pdftools)
+# load the following libraries ----
+library(pdftools)			
 library(data.table)
 library(tidyverse)
 library(plyr)
@@ -9,31 +10,39 @@ library(xlsx)
 # dyn.load('/Library/Java/JavaVirtualMachines/jdk-11.0.2.jdk/Contents/Home/jre/lib/server/libjvm.dylib')
 library(rJava)
 
+# set working directory where the village pdf files are located ----
 parentFolder <- setwd("/Users/asoc/Desktop/CSISA_2019/CSISA_IN/AP_KVK/Anantapur/Vankarakunta")
+
+# first you need to change the pdf format to png if the files are not extractable ----
+filenames <- list.files(parentFolder, pattern = "*.pdf$")
+
 filenames <- list.files(parentFolder, pattern = "*.png$")
 filenames
 z <- mixedsort(sort(filenames))
-z
+y <- sapply(z, pdftools::pdf_convert, dpi=300)
 
-text <- sapply(z, tesseract::ocr)
-
+# after you have changed the files to png ----
+filenames <- list.files(parentFolder, pattern = "*.png$")
+filenames
+text <- sapply(y, tesseract::ocr)
 text
 
+# look for the pattern of "House Number" in the extracted text
 w <- str_extract_all(text, "House Number : [[0-9][A-O][—][/][-][ ][.][~]]{1,9} | House Number [[0-9][A-O][—][/][-][ ][.][~]]{1,9}
                      | House Number: [[0-9][A-O][—][/][-][ ][.][~]]{1,9} | H NO [[0-9][A-O][—][/][-][ ][.][~]]{1,9}")
 w
+
 u <- str_extract_all(w, "[[A][I][B][C][D][E][F][0-9][—][/][-][ ][.][-]]+")
+
 u
+
 x <- lapply(u, unique)
 x
-
 
 for (i in seq_along(x)) {
   filename = paste(z[[i]],".csv", sep = "")
   write.table(x[i],filename, row.names = TRUE, col.names = "hhID")
 }
-
-
 
 
 files <- list.files(pattern="*.csv$")
@@ -62,15 +71,10 @@ write.csv(q,"Vankarakunta.csv", row.names = FALSE)
 
 
 
-
-
-# combining all final random csv files
-
+# combining all final random csv files ----
 
 parentFolder <- "/Users/asoc/Desktop/CSISA_2019/CSISA_IN/AP_KVK/Anantapur"
 setwd(parentFolder)
-
-# do.call(file.remove, list(list.files(parentFolder, full.names = T, pattern = "*.csv$", recursive = T)))
 
 files <- list.files(path = parentFolder, full.names = T, recursive = T, pattern = "^[a-zA-Z_]+.csv")
 files
@@ -88,5 +92,3 @@ for (i in allVill) {
 }
 
 saveWorkbook(wb, "finalRandomNumberAnantapur.xlsx")
-
-
