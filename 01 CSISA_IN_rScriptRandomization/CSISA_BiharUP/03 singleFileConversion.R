6395db010d82dfb4d2fabb670536d24d01a81b75
CSISA Project <52654890+CSISAProject@users.noreply.github.com>
2019-07-17 14:19:07 +0545

    Update 03 singleFileConversion.R
    Added comments

--- 01 CSISA_IN_rScriptRandomization/CSISA_BiharUP/03 singleFileConversion.R
+++ 01 CSISA_IN_rScriptRandomization/CSISA_BiharUP/03 singleFileConversion.R
@@ -1,22 +1,15 @@
 
-
+# make sure that you have loaded all the required packages
+# also make sure that the files are placed according to the file pathnames
 
 parentFolder <- "/Users/apple/Desktop/SurveySampling/Bihar_UP/01restDistricts/Balia/29_NaukaGaon"
 setwd(parentFolder)
-
 filenames <- list.files(parentFolder, pattern = "*.pdf$")
-
-
-
 filenames
-
 z <- mixedsort(sort(filenames))
-
 y <- sapply(z, pdf_text)
 
-
-
-# ************************************************** #
+# the follwing is an example from PDFs of Uttar Pradesh
 
 w <- str_extract_all(y, "मकपन सगखनप : \\d{1,8} | मकपन सभखयप : \\d{1,8} | मकपन सगखयप : \\d{1,8} | मकपन सपखयप : \\d{1,8}  | मकपन सपखनप : \\d{1,8} | मककन सभखनक : \\d{1,8}  | 
                      मकपन सभखनप : \\d{1,8} | मसहन सगखनह : \\d{1,8} | मसहन सभखनह : \\d{1,8} |
@@ -26,7 +19,6 @@ w <- str_extract_all(y, "मकपन सगखनप : \\d{1,8} | मकपन 
                      मककन सपखनक : \\d{1,8} | मकपन सवखनप : \\d{1,8} | मकमन सपखनम : \\d{1,8}")
 
 u <- str_extract_all(w, "[0-9.]+")
-
 
 x <- lapply(u, unique)
 x <- lapply(x, as.integer)
@@ -40,4 +32,3 @@ for (i in seq_along(dt)) {
   write.table(dt[i],filename, row.names = FALSE, col.names = "hhID")
   
 }
-
